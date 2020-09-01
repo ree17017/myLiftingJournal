34e884e68e80af3ed97ddce7654e842fe9cd7f6d
Joshua Reed <joshuareed@workfront.com>
2020-09-01 09:26:29 -0600

    Login and Registration Form

---
+++ src/components/RegistrationForm/RegistrationForm.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button'
+import styled from 'styled-components'
+
+function RegistrationForm(props) {
+    return (
+        
+    );
+}
